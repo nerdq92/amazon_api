00046269d25e362dee29e4d1a54ae6b8c9d8f50e
nerdq92 <90452708+nerdq92@users.noreply.github.com>
2024-11-07 18:16:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,28 +3,29 @@ from bs4 import BeautifulSoup
 import requests
 import json
 
-def fetch_open_graph_data(url):
+def fetch_books_data(url):
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Connection": "keep-alive",
     }
-    # headers = {
-    #     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
-    #     "Accept-Language": "en-US,en;q=0.9",
-    #     "Accept-Encoding": "gzip, deflate, br",
-    #     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-    #     "Connection": "keep-alive",
-    # }
-    response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    # Extract Open Graph properties
-    tag = soup.find('img', {'id': 'landingImage'})
-    if tag and tag.has_attr('data-a-dynamic-image'):
-        data_dynamic_image = json.loads(tag['data-a-dynamic-image'])
-        title = tag['alt']
-        image_url = list(data_dynamic_image.keys())[0]
-    return soup
-try:
-    soup = fetch_open_graph_data('https://www.amazon.in/Complete-Novels-Sherlock-Holmes/dp/8175994312/ref=zg_bs_g_1318054031_d_sccl_1/000-0000000-0000000?psc=1')
-    st.write(soup)
-except:
-    st.write('Error occur')
+    response = requests.get(url,headers=headers)
+    data = BeautifulSoup(response.content, 'html.parser')
+    # img_tag = data.find("img", class_="img-responsive mx-auto d-block swiper-lazy")
+    # data_image = img_tag.get("data-image")
+    # alt_text = img_tag.get("alt")
+    # return data_image,alt_text
+    return data
+
+st.title("Book Search")
+url = st.text_input("Enter a site url")
+
+if st.button("Search"):
+    # book_title, image_url = fetch_books_data(url)
+    # data_image,alt_text = fetch_books_data(url)
+    data = fetch_books_data(url)
+    st.write(data)
+    # st.write(alt_text)
+    # st.image('https:'+data_image,width=250)    
